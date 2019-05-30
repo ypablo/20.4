d44bcba4f5c63867288d2f914dca183a0bc21f7b
ypablo <yfedora@gmail.com>
2019-05-30 17:21:34 +0100

    Webpack + react

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 class Title extends React.Component {
-   
+
 
     render() {
         return (
             <div>
                 <h1>To Do List</h1>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         );
     }
-    
+
 }
 export default Title;
