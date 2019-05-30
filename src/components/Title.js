81cbf1fe103930ef58bde8883e9599312fe9e297
ypablo <yfedora@gmail.com>
2019-05-30 17:41:29 +0100

    Webpack + react

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import style from '../containers/App.css';
 
 class Title extends React.Component {
 
 
     render() {
         return (
-            <div>
+            <div className={style.Title}>
                 <h1>To Do List</h1>
                 <ul>
                     <li></li>
